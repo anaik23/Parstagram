8621e01c7acb5ca9ebab80b2e386bf036b8a9b81
Abhay Naik <abhay.naik@hotmail.com>
2020-09-30 16:39:34 -0700

    first commit

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,9 +14,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration:
+                ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Parstagram"
+                configuration.server = "https://protected-stream-97804.herokuapp.com/parse"
+            })
+        )
+        
         return true
+        
     }
+    
+   
 
     // MARK: UISceneSession Lifecycle
 
